a3c0f257149c91e367840d4c782f6d17f1fdb8c3
Michael Talarico <94918808+mtalarico@users.noreply.github.com>
2022-07-11 14:35:34 -0400

    Update main.rs
    changed variable name for clarity

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let uri = format!(
         "mongodb+srv://{USERNAME}:{PASSWORD}@{CLUSTERNAME}.{PROJECT_HASH}.mongodb.net/?retryWrites=true&w=majority"
     );
-    let dal = DB::connect(uri.as_str()).await?;
-    dal.insert_standalone().await?;
-    dal.insert_bucketed().await?;
+    let db_client = DB::connect(uri.as_str()).await?;
+    db_client.insert_standalone().await?;
+    db_client.insert_bucketed().await?;
 
     return Ok(());
 }
